868a37a8bdc6c01f18654c26bff6635630ff958b
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-13 00:57:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -12,7 +12,8 @@ end
 
 #my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |language| language.upcase}
 #my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) { |student| st#udent.split("").first}
-[languages = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect([languages]) { |language| language.upcase}
-my_collect([students]) { |student| st#udent.split("").first}
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+my_collect(languages) { |language| language.upcase}
+my_collect(students) { |student| st#udent.split("").first}
 
