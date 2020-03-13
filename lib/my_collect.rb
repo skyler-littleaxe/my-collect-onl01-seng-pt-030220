73c4eec8c089e87921d3f1f138e1e30c00da0642
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-13 00:59:53 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  #if block_given?
+    i = 0
+    collect = []
+    while i < array.length
+    collect <<
+    yield(array[i])
+    i += 1
+  end
+  collect
+end
 
+#my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |language| language.upcase}
+#my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) { |student| st#udent.split("").first}
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+my_collect(languages) { |language| language.upcase}
+my_collect(students) { |student| student.split("").first}
 
